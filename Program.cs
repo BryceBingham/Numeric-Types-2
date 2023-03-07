aa255b457c97f17a4985c59f4c8869058b7dbdd7
BryceBingham <brycebingham04@gmail.com>
2023-03-07 13:32:00 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumericTypes;
+
+public class AreaOfRectangle
+{
+    public static void Main()
+    {
+            Console.WriteLine("Enter Length:");
+            int Length = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine("Enter Height:");
+            int Height = Convert.ToInt32 (Console.ReadLine());
+
+            int Area = Length * Height;
+            Console.WriteLine("The area of the rectangle is " + Area);
+            Console.ReadKey();
+    }
+}
